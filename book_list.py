bedcc0e81f46982ddc52ed6fdd536bae4dc34c82
guyuedumingx <guyuedumingx@gmail.com>
2021-03-08 12:08:08 +0800

    update

--- book_list.py
+++ book_list.py
@@ -1,3 +1,5 @@
+import json
+
 class Book_List:
 
     def __init__(self, data):
@@ -7,6 +9,10 @@ class Book_List:
         self.intro = data['intro']
         self.kind = data['kind']
         self.cover_url = data['cover_url']
+    
+    def to_json(self):
+        return json.dumps(self.__dict__, ensure_ascii=False)
+
 
 class Book_Info:
 
@@ -16,7 +22,23 @@ class Book_Info:
         self.intro = data['intro']
         self.kind = data['kind']
         self.cover_url = data['cover_url']
-        self.toc_url = data['toc_url']
         self.last_chapter = data['last_chapter']
+        self.toc_info = data['toc_info']
+
+    def get_chapter_url_by_no(self, no):
+        return self.toc_info[no-1]['url']
+    
+    def get_chapter_url_by_name(self, book_name):
+        for chapter in self.toc_info:
+            if(book_name == chapter['name']):
+                return chapter['url']
+        return ""
+
+    def save(self, file):
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(json.dumps(self.__dict__, ensure_ascii=False))
+
+    def to_json(self):
+        return json.dumps(self.__dict__, ensure_ascii=False)
 
 
--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import requests
+from html.parser import HTMLParser
 from lxml import etree
 import json
 import re
@@ -10,9 +11,12 @@ class Downloader:
     def __init__(self, rules):
         self.rule_search = rules['ruleSearch']
         self.rule_book_info = rules['ruleBookInfo']
+        self.rule_toc = rules['ruleToc']
+        self.rule_content = rules['ruleContent']
         self.method = rules['method']
         self.key = "圣墟"
         self.charset = rules['charset']
+        self.encoding = rules['charset']
         self.url = rules['searchUrl']
         self.sourceUrl = rules['bookSourceUrl']
         self.data = {}
@@ -48,6 +52,7 @@ class Downloader:
         else:
             response = requests.get(self.url, headers=self.headers, params=self.data)
             response.encoding = response.apparent_encoding
+            self.encoding = response.encoding
             return response.text
 
 
@@ -64,23 +69,64 @@ class Downloader:
                 data['intro'] = book_list.xpath(self.rule_search['intro'])[0]
                 data['kind'] = book_list.xpath(self.rule_search['kind'])[0]
                 data['cover_url'] = book_list.xpath(self.rule_search['coverUrl'])[0]
-                data['book_url'] = self.fix_book_url(data['book_url'])
+                data['book_url'] = self.fix_url(data['book_url'])
                 book = Book_List(data)
                 book_lists.append(book)
             except:
                 continue
         return book_lists
         
-    def fix_book_url(self, book_url):
+    def fix_url(self, book_url):
         if(book_url.find("http") == -1):
             return self.sourceUrl + book_url
 
     def get_book_info(self, book):
-        self.data = {}
+        data = {}
         self.url = book.book_url
         html_text = self.get_html()
-        html = etree.HTML(html_text)
-        books = html.xpath(self.rule_search['bookList'])
+        info = etree.HTML(html_text)
+        data['author'] = info.xpath(self.rule_book_info['author'])[0]
+        data['name'] = info.xpath(self.rule_book_info['name'])[0]
+        data['intro'] = info.xpath(self.rule_book_info['intro'])[0]
+        data['kind'] = info.xpath(self.rule_book_info['kind'])[0]
+        data['cover_url'] = info.xpath(self.rule_book_info['cover_url'])[0]
+        data['last_chapter'] = info.xpath(self.rule_book_info['last_chapter'])[0]
+        data['toc_url'] = info.xpath(self.rule_book_info['toc_url'])[0]
+        toc_info = self.get_toc_info(data['toc_url'])
+        data['toc_info'] = toc_info
+        book = Book_Info(data) 
+        return book
+    
+    def get_toc_info(self, toc):
+        chapter_infos = []
+        chapter_list = toc.xpath(self.rule_toc['chapterList'])
+        for chapter in chapter_list:
+            data = {}
+            data['name'] = chapter.xpath(self.rule_toc['chapterName'])[0]
+            data['url'] = chapter.xpath(self.rule_toc['chapterUrl'])[0]
+            data['url'] = self.fix_url(data['url'])
+            chapter_infos.append(data)
+        if(self.rule_toc['nextTocUrl'] != ""):
+            pass
+        return chapter_infos
+    
+    def get_chapter_info(self, chapter_url):
+        self.url = chapter_url
+        self.data = {}
+        html_text = self.get_html()
+        info = etree.HTML(html_text)
+        content_html = info.xpath(self.rule_content['content'])[0]
+        content = etree.tostring(content_html, pretty_print=True, method='html').decode(self.encoding)
+        if(self.rule_content['nextContentUrl'] != ""):
+            pass
+        return self.decodeHtml(content)
+
+    def decodeHtml(self, input):
+        h = HTMLParser()
+        s = h.unescape(input)
+        return s
+
+
 
 
 with open("rules.json", "r", encoding="utf-8") as f:
@@ -88,5 +134,14 @@ with open("rules.json", "r", encoding="utf-8") as f:
 downloader = Downloader(rules[0])
 text = downloader.get_html()
 book_list = downloader.get_book_list(text)
-print(book_list)
+book_info = downloader.get_book_info(book_list[0])
+chapter_url = book_info.get_chapter_url_by_no(1)
+content = downloader.get_chapter_info(chapter_url)
+books = []
+for book in book_list:
+    books.append(book.to_json())
+
+with open("books.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(books))
+book_info.save("book_info.json")
 
